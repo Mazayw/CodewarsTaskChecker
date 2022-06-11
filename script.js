a00044132b1a366bde8ab729004b46ac9461a5b1
Mazayw <mazayw@gmail.com>
2022-06-11 20:51:43 +0300

    feat: add errors handling

--- script.js
+++ script.js
@@ -11,56 +11,72 @@ let page = 0;
 let userKatas = [];
 
 button.addEventListener('click', () => {
-  done = [];
-  undone = [];
-  page = 0;
-  userKatas = [];
-  doneTasks.innerHTML = '';
-  undoneTasks.innerHTML = '';
+  if (name.value.length === 0 || links.value.length === 0) {
+    alert('Fill all fields');
+  } else {
+    done = [];
+    undone = [];
+    page = 0;
+    userKatas = [];
+    doneTasks.innerHTML = '';
+    undoneTasks.innerHTML = '';
 
-  const allLinksArr = links.value
-    .split(' ')
-    .filter((el) => el.startsWith('http'));
+    const allLinksArr = links.value
+      .split(' ')
+      .filter((el) => el.includes('codewars.com/kata'));
 
-  const slugs = [];
+    const slugs = [];
 
-  allLinksArr.forEach((el) => {
-    const strEnd = el.indexOf('\n') !== -1 ? el.indexOf('\n') : el.length;
-    const slug = el.slice(el.indexOf('kata') + 4, strEnd);
-    slugs.push(slug.replaceAll(/\//gi, ''));
-  });
-
-  getUserKatas(name.value, page, slugs);
+    allLinksArr.forEach((el) => {
+      const strEnd = el.indexOf('\n') !== -1 ? el.indexOf('\n') : el.length;
+      const slug = el.slice(el.indexOf('kata') + 4, strEnd);
+      slugs.push(slug.replaceAll(/\//gi, ''));
+    });
+    if (slugs.length === 0) {
+      alert('Enter some Codewars katas links first');
+    } else {
+      getUserKatas(name.value, page, slugs);
+    }
+  }
 });
 
-function getUserKatas(user, page, slugs) {
-  fetch(
-    `https://www.codewars.com/api/v1/users/${user}/code-challenges/completed?page=${page}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      userKatas = [...userKatas, ...data.data];
-      page++;
-      if (page < data.totalPages) {
-        getUserKatas(user, page);
-      } else {
-        userKatas.forEach((el) => {
-          if (slugs.includes(el.slug)) done.push(el.slug);
-        });
+function getUserKatas(user, page, slugsData) {
+  try {
+    fetch(
+      `https://www.codewars.com/api/v1/users/${user}/code-challenges/completed?page=${page}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success === false) {
+          alert('You entered wrong name. Check the upper and lower case');
+        } else {
+          userKatas = [...userKatas, ...data.data];
+          page++;
 
-        slugs.forEach((el) => {
-          if (!done.includes(el)) undone.push(el);
-        });
+          if (page < data.totalPages) {
+            getUserKatas(user, page, slugsData);
+          } else {
+            userKatas.forEach((el) => {
+              if (slugsData.includes(el.slug)) done.push(el.slug);
+            });
 
-        done.forEach((el) => {
-          createHtml(el, doneTasks, 'Done Katas');
-        });
+            slugsData.forEach((el) => {
+              if (!done.includes(el)) undone.push(el);
+            });
 
-        undone.forEach((el) => {
-          createHtml(el, undoneTasks, 'Undone Katas');
-        });
-      }
-    });
+            done.forEach((el) => {
+              createHtml(el, doneTasks, 'Done Katas');
+            });
+
+            undone.forEach((el) => {
+              createHtml(el, undoneTasks, 'Undone Katas');
+            });
+          }
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function createHtml(slug, item, headerText) {
